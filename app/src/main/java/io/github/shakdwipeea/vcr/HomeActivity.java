bc0b5bdac9733675c61192fa2eef815250cf2994
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2016-07-04 00:10:43 +0530

    download in public folder fix #3 and fix #1

--- app/src/main/java/io/github/shakdwipeea/vcr/HomeActivity.java
+++ app/src/main/java/io/github/shakdwipeea/vcr/HomeActivity.java
@@ -1,11 +1,16 @@
 package io.github.shakdwipeea.vcr;
 
+import android.Manifest;
 import android.app.DownloadManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
@@ -20,15 +25,17 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.OnTextChanged;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class HomeActivity extends AppCompatActivity implements View.OnKeyListener, Callback<Song>, View.OnClickListener {
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 100;
+
     @BindView(R.id.search_query) EditText searchQuery;
     @BindView(R.id.progress_bar) ProgressBar progressBar;
 
@@ -37,6 +44,8 @@ public class HomeActivity extends AppCompatActivity implements View.OnKeyListene
     @BindView(R.id.download_button) Button downloadBtn;
 
     Song song;
+
+    public static final String MUSIC_DIR = "/vcr_music";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,7 +76,14 @@ public class HomeActivity extends AppCompatActivity implements View.OnKeyListene
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_rate) {
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + this.getPackageName())));
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Could not open play Store", Toast.LENGTH_SHORT)
+                        .show();
+            }
             return true;
         }
 
@@ -84,6 +100,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnKeyListene
      */
     @Override
     public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+
         if (keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
             switch (keyCode) {
                 case KeyEvent.KEYCODE_DPAD_CENTER:
@@ -142,26 +159,40 @@ public class HomeActivity extends AppCompatActivity implements View.OnKeyListene
 
 
     private void displaySongDetails() {
-        songName.setText(getSongName(song.getUrl()));
+        songName.setText(getSongName());
         downloadBtn.setOnClickListener(this);
         resultCard.setVisibility(View.VISIBLE);
+        downloadBtn.setEnabled(true);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.download_button:
+                downloadBtn.setEnabled(false);
                 downloadSong();
                 break;
             default:break;
         }
     }
 
-    private String getSongName(String songUrl) {
-        return songUrl.split("/")[2];
+    private String getSongName() {
+        return song.getUrl().split("/")[2];
     }
 
     private void downloadSong() {
+
+        if (!checkPerms()) {
+            return;
+        }
+
+        File direct = new File(Environment.getExternalStorageDirectory()
+                + MUSIC_DIR);
+
+        if (!direct.exists()) {
+            direct.mkdirs();
+        }
+
         DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
         Uri source = Uri.parse(ServiceFactory.BASE_URL)
                 .buildUpon()
@@ -169,11 +200,72 @@ public class HomeActivity extends AppCompatActivity implements View.OnKeyListene
                 .build();
 
         DownloadManager.Request request = new DownloadManager.Request(source);
-        request.setTitle(getSongName(song.getUrl()));
+        request.setTitle(getSongName());
         request.setDescription("Downloading song from VCR");
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
         request.allowScanningByMediaScanner();
+        request.setDestinationInExternalPublicDir(MUSIC_DIR, getSongName());
 
         dm.enqueue(request);
+
+        Toast.makeText(this, "Download will start shortly", Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    downloadSong();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "No permissions", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                return;
+
+            }
+        }
+    }
+
+    private boolean checkPerms() {
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+
+            return false;
+        } else {
+            return true;
+        }
     }
 }
